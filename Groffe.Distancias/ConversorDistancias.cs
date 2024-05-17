083a37431bc578147ae7faa4f2001c5a60c5db06
Renato Groffe <renato.groffe@yahoo.com.br>
2024-05-17 19:31:39 -0300

    Correcao de bug

--- Groffe.Distancias/ConversorDistancias.cs
+++ Groffe.Distancias/ConversorDistancias.cs
@@ -3,10 +3,10 @@
 public class ConversorDistancias
 {
     public static double MilhasParaKm(double milhas) =>
-        milhas * 1.609; // TODO: Retirar essa simulação de falha
-        //Math.Round(milhas * 1.609, 3);
+        //milhas * 1.609; // TODO: Retirar essa simulação de falha
+        Math.Round(milhas * 1.609, 3);
 
     public static double KmParaMilhas(double km) =>
-        km / 1.609; // TODO: Retirar essa simulação de falha
-        //Math.Round(km / 1.609, 3);
+        //km / 1.609; // TODO: Retirar essa simulação de falha
+        Math.Round(km / 1.609, 3);
 }
